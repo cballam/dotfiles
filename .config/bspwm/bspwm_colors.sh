1a3b6876432584bf57f1a45d395b01b1cb0a21b5
cballam <cole.ballam@gmail.com>
2021-01-01 20:43:14 +1000

    sxhkd update, bspwm colors added
    Modifications to sxhkd bindings for floating window movements as well as
    addition of bspwm_colors file for window outlines

--- .config/bspwm/bspwm_colors.sh
+++ .config/bspwm/bspwm_colors.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-bspc config focused_border_color "#3364b8"
-bspc config normal_border_color  "#23457e"
+bspc config focused_border_color "#A0F1F1F1"
+bspc config normal_border_color  "#354359"
